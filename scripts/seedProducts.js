39b0455da61f32d7d740959fade39438671f9e62
shreyash-leo <shreyashshirsikar@gmail.com>
2025-12-19 17:15:36 +0530

    Update Dell laptop seed data with correct specs and pricing

--- scripts/seedProducts.js
+++ scripts/seedProducts.js
@@ -4,38 +4,39 @@ const Product = require('../models/Product');
 
 const products = [
   {
-    name: 'Dell Latitude 5320 13-inch (i5 11th Gen, 16GB, 256GB)',
+    name: 'Dell Latitude 5320 13.3"',
     price: 32499,
     originalPrice: 45999,
     image:
       'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/13-5320/media-gallery/notebook-latitude-13-5320-t-gallery-1.psd',
-    condition: 'Excellent',
+    condition: 'Refurbished - Excellent',
     category: 'laptops',
     brand: 'Dell',
     description:
-      'Dell Latitude 5320 13-inch laptop with Intel Core i5 11th Gen, 16GB RAM and 256GB SSD. Ideal for business and professional use.',
+      'Dell Latitude 5320 with Intel Core i5 11th Gen, 16GB RAM, 256GB SSD. Fully tested and professionally refurbished.',
     specs: [
       'Intel Core i5 11th Gen',
       '16GB DDR4 RAM',
       '256GB SSD',
-      '13-inch Full HD Display',
-      'Windows 11 Pro'
+      '13.3-inch Full HD Display',
+      'Windows 11 Pro',
+      'Backlit Keyboard'
     ],
     warranty: '2 Years Rekraft Warranty'
   },
   {
-    name: 'Dell Latitude 7400 14-inch (i7, 8GB, 256GB)',
+    name: 'Dell Latitude 7400 14"',
     price: 28999,
     originalPrice: 42999,
     image:
       'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/14-7400/media-gallery/notebook-latitude-14-7400-gallery-1.psd',
-    condition: 'Excellent',
+    condition: 'Refurbished - Excellent',
     category: 'laptops',
     brand: 'Dell',
     description:
-      'Dell Latitude 7400 14-inch laptop powered by Intel Core i7 processor with premium design and performance.',
+      'Dell Latitude 7400 powered by Intel Core i7 processor with premium build quality.',
     specs: [
-      'Intel Core i7 Processor',
+      'Intel Core i7 8th Gen',
       '8GB DDR4 RAM',
       '256GB SSD',
       '14-inch Full HD Display',
@@ -44,38 +45,38 @@ const products = [
     warranty: '2 Years Rekraft Warranty'
   },
   {
-    name: 'Dell Latitude 5490 14-inch (i5, 8GB, 256GB)',
+    name: 'Dell Latitude 5490 14"',
     price: 25499,
     originalPrice: 38999,
     image:
       'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/14-5490/media-gallery/notebook-latitude-14-5490-gallery-1.psd',
-    condition: 'Very Good',
+    condition: 'Refurbished - Very Good',
     category: 'laptops',
     brand: 'Dell',
     description:
-      'Dell Latitude 5490 with Intel Core i5 processor, suitable for office, students and everyday business tasks.',
+      'Reliable Dell Latitude 5490 with Intel Core i5 processor, ideal for office and business use.',
     specs: [
-      'Intel Core i5 Processor',
+      'Intel Core i5 8th Gen',
       '8GB RAM',
       '256GB SSD',
-      '14-inch Display',
+      '14-inch HD Display',
       'Windows 11 Pro'
     ],
     warranty: '1 Year Rekraft Warranty'
   },
   {
-    name: 'Dell Latitude 5480 14-inch (i7, 8GB, 256GB)',
+    name: 'Dell Latitude 5480 14"',
     price: 24999,
     originalPrice: 36999,
     image:
       'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/14-5480/media-gallery/notebook-latitude-14-5480-gallery-1.psd',
-    condition: 'Very Good',
+    condition: 'Refurbished - Very Good',
     category: 'laptops',
     brand: 'Dell',
     description:
-      'Dell Latitude 5480 with Intel Core i7 processor offering solid performance and durability.',
+      'Dell Latitude 5480 featuring Intel Core i7 processor with solid performance and durability.',
     specs: [
-      'Intel Core i7 Processor',
+      'Intel Core i7 7th Gen',
       '8GB RAM',
       '256GB SSD',
       '14-inch Full HD Display',
